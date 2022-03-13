8262ac21e53029489f37d04831bb01490d29fb27
matthew chen <matthewchen@ucla.edu>
2022-03-13 15:02:59 -0400

    move color constants to separate file

--- src/shared/utils/constants.ts
+++
@@ -1,3 +0,0 @@
-export const gray1 = '#F8F8F8';
-
-export const white = '#FFFFFF';
---
+++ src/shared/utils/styles/colors.ts
@@ -0,0 +1,11 @@
+export const grayLight = '#F8F8F8';
+export const grayMedium = '#EAEAEA';
+
+export const blueLight = '#2A2D38';
+export const blueMedium = '#252932';
+export const blueDark = '#22252E';
+
+export const white = '#FFFFFF';
+export const black = '#000000';
+
+export const orange = '#FF9F0B';
